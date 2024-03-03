58663555267342f1b1626b9e0eb9fbc94c8fc67c
Sai Kiran <skiran.mekala@gmail.com>
2024-03-03 18:39:21 +0000

    creating home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Tab,
+  Tabs,
+  Typography,
+  Toolbar,
+} from "@mui/material";
+import { Add, Category, Storefront, Inventory2 } from "@mui/icons-material";
+// import ManageCategory from "./pages/manageCategory";
+// import ManageStock from "./pages/manageStock";
+// import AddStockForm from "./components/AddStock";
+// import StorePage from "./pages/manageStore";
+// import { useDataContext } from "./context/DataContext";
+import "./App.css";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div>
+            <AppBar position="static">
+              <Toolbar>
+                <Typography variant="h6">Home</Typography>
+              </Toolbar>
+            </AppBar>
     </div>
-  );
-}
+  )
+};
 
 export default App;
