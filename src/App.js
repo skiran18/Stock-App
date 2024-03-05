07a76cc979bfc48ea538beecc4a7c7740dac9b9b
Sai Kiran <skiran.mekala@gmail.com>
2024-03-05 07:05:26 +0000

    adding store to the manage store

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import {
   Button,
   Typography,
   Toolbar,
+  Card, CardContent
 } from "@mui/material";
 import Box from '@mui/material/Box';
+import { FormControl, FormControlLabel } from '@mui/material';
+import { RadioGroup, Radio } from '@mui/material';
 
 import "./App.css";
+
+const stores = [
+  { id: 1, name: 'Store 1', description: 'Description for Store 1' },
+  { id: 2, name: 'Store 2', description: 'Description for Store 2' },
+  { id: 3, name: 'Store 3', description: 'Description for Store 3' }
+];
+
 const sideNavItems = ['Stores', 'Categories', 'Stock']
 const App = () => {
   const [page, setPage] = useState('');
@@ -15,6 +25,16 @@ const App = () => {
   const handleOption = (i) => {
     setPage(i)
   }
+
+  const [selectedStore, setSelectedStore] = useState(stores[0]);
+
+  const handleStoreChange = (event) => {
+    const storeId = parseInt(event.target.value);
+    const selected = stores.find(store => store.id === storeId);
+    setSelectedStore(selected);
+  };
+
+
   return (
           <div>
             <AppBar position="static">
@@ -35,6 +55,18 @@ const App = () => {
           </Box>
         </Toolbar>
             </AppBar>
+            <FormControl component="fieldset">
+        <RadioGroup aria-label="store" name="store" value={selectedStore.id.toString()} onChange={handleStoreChange}>
+          {stores.map(store => (
+            <Card key={store.id} style={{ margin: '8px', width:'100%' }}>
+              <CardContent>
+                <FormControlLabel value={store.id.toString()} control={<Radio />} label={store.name} />
+                <Typography variant="body1">{store.description}</Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </RadioGroup>
+      </FormControl>
     </div>
   )
 };
